1fb9b6f0079bfc7f0256f55b93cd32e1b0487527
zetaleo <alaverdi17@mail.ru>
2021-08-03 00:54:56 +0300

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const links = [
     url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free! Good luck!",
     color: "#663399",
   },
 ]
